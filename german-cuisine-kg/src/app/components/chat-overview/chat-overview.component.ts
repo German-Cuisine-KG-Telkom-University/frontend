ec123acfcc3edcc5c3d7cc112a08ee5a6edb1d35
mfnomad <mfnomad@gmail.com>
2025-08-18 08:01:18 +0200

    loading animation during query processing

--- german-cuisine-kg/src/app/components/chat-overview/chat-overview.component.ts
+++ german-cuisine-kg/src/app/components/chat-overview/chat-overview.component.ts
@@ -5,16 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../../services/chat.service';
-
+import { ReponseLoaderComponent } from '../reponse-loader/reponse-loader.component';
 @Component({
   selector: 'app-chat-overview',
   templateUrl: './chat-overview.component.html',
   styleUrls: ['./chat-overview.component.css'],
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, ReponseLoaderComponent]
 })
 export class ChatOverviewComponent {
   @Input() messages: Message[] = [];
   currentInput: string = '';
+  loading: boolean = false;
 
   constructor(private chatService: ChatService) {}
 
@@ -29,13 +30,20 @@ sendMessage() {
 
   const input = this.currentInput;
   this.currentInput = '';
+  this.loading = true;
 
-  this.chatService.sendMessage(input).subscribe((replyMessage: Message) => {
-    console.log('In compoenent, received reply:', replyMessage);
-    this.messages.push({
-      sender: 'right',
-      answer: replyMessage.answer
+this.chatService.sendMessage(input).subscribe({
+      next: (replyMessage: Message) => {
+        console.log('In component, received reply:', replyMessage);
+        this.messages.push({
+          sender: 'right',
+          answer: replyMessage.answer
+        });
+        this.loading = false;   
+      },
+      error: () => {
+        this.loading = false;   
+      }
     });
-  });
-}
+  }
 }
